8da78412b3138c8f8eb660f88e06b8785ce2663a
JosephNjoroge8 <joem7mach@gmail.com>
2024-09-02 16:38:54 -0700

    update on starwar API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // prints the title of a Star Wars movie where the episode number
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+const starWarsUrl = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(starWarsUri, function (_err, _res, body) {
+request(starWarsUrl, function (_err, _res, body) {
   body = JSON.parse(body);
   console.log(body.title);
-});`
+});
